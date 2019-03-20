e9f594add06a864207380257179c00751e399821
unknown <milosz1092@gmail.com>
2019-03-20 08:22:13 +0100

    Uodate

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { IUserLoginRequest } from './IUserLoginRequest';
 import { IUserLoginResponse } from './IUserLoginResponse';
+import { IUserAuthorizationResponse } from './IUserAuthorizationResponse';
 
-export { IUserLoginRequest, IUserLoginResponse };
+export { IUserLoginRequest, IUserLoginResponse, IUserAuthorizationResponse };
